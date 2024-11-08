a89e073761af4eeb0fa80fb06b75b5c953d24146
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-11-08 17:07:18 +0100

    improve build script

--- build.sh
+++ build.sh
@@ -1,7 +1,68 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-MY_DIR=`dirname "$(readlink -f "$0")"`
+set -eu
+
+# Default values
+image_file="image.ext2"
+size="1500M"
+
+# Help message function
+print_help() {
+  echo "Usage: $0 [options] [<image_file>]"
+  echo ""
+  echo "Options:"
+  echo "  --size, -s      Specify the size (default: 1500M)"
+  echo "  --help, -h      Display this help message"
+  echo ""
+  echo "Positional Arguments:"
+  echo "  <image_file>    Path of the image file (default: ./image.ext2)"
+}
+
+# Parse options
+while getopts ":s:h-:" opt; do
+  case $opt in
+    s) size="$OPTARG" ;;
+    h) print_help; exit 0 ;;
+    -)
+      case $OPTARG in
+        size) size="${!OPTIND}"; OPTIND=$((OPTIND + 1)) ;;
+        help) print_help; exit 0 ;;
+        *)
+          echo "Invalid option: --$OPTARG" >&2
+          exit 1
+          ;;
+      esac
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+# Positional argument
+if [ $# -gt 0 ]; then
+  image_file="$1"
+fi
+
+echo "Image file: $image_file"
+echo "Size: $size"
+
+# Privileged section running in a namespace
+buildah unshare bash << EOF
 
 buildah bud --layers -t alpine_root -f Dockerfile
 
-buildah bud --layers -t export -f export.dockerfile -v ${MY_DIR}:/export
+alpine_root_cnt=\$(buildah from alpine_root)
+mnt=\$(buildah mount \$alpine_root_cnt)
+cleanup() {
+	buildah umount \$alpine_root_cnt
+	buildah rm \$alpine_root_cnt
+}
+trap cleanup EXIT
+
+rm -f ${image_file}
+mkfs.ext2 -b 4096 -d \$mnt ${image_file} ${size}
+
+EOF
